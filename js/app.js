693e9736fe2eb9ed55364c0939d5b8f46f3a8890
isahua16 <isaelhuard@gmail.com>
2023-03-21 20:33:12 -0600

    playing with functions

--- js/app.js
+++ js/app.js
@@ -19,4 +19,26 @@ function generic_function()
 }
 generic_function();
 
+function specific_greeting(str)
+{
+    console.log(`Hello ${str}`);
+}
 
+specific_greeting(`isael`);
+specific_greeting(`amy`);
+specific_greeting(`claudine`);
+
+function find_specific_user(arr, user)
+{
+    for(let i = 0; i < arr.length; i++)
+    {
+        if(arr[i].includes(user))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+let users = [`isael`, `greedo`, `han`];
+let contain_admin = find_specific_user(users, `admin`);
